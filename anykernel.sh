04d638fbe8e8add869b8f36bc9b5fd346a275b5c
Kazera62 <kemanaapa207@gmail.com>
2025-08-05 10:22:07 +0700

    Update Name anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=by belowzeroiq @ github
+kernel.string=by Kazera62 @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=0
